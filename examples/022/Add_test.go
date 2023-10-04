1ea47ea0b95a602613d9fb4b1db1c7902b9e4fe3
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2023-10-04 21:43:38 +0800

    Add files via upload

---
+++ examples/022/Add_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	// Test cases
+	cases := []struct {
+		a, b, want int
+	}{
+		{1, 2, 3},
+		{0, 0, 0},
+		{-1, 1, 0},
+		{-1, -1, -2},
+	}
+	// Iterate over test cases
+	for _, c := range cases {
+		got := Add(c.a, c.b)
+		if got != c.want {
+			t.Errorf("Add(%d, %d) == %d, want %d", c.a, c.b, got, c.want)
+		}
+	}
+}
